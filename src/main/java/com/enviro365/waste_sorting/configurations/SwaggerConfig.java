0efc1f0c4f397b87c4c309bd07469015a659eecd
WellingtonMpofu <wellington.t.mpofu@gmail.com>
2024-06-04 01:04:29 +0200

    added swagger config

---
+++ src/main/java/com/enviro365/waste_sorting/configurations/SwaggerConfig.java
@@ -0,0 +1,17 @@
+package com.enviro365.waste_sorting.configurations;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info().title("Enviro365 APIs").version("1.0").description("Waste Sorting APIs that provide guidance on proper waste disposal techniques and recycling tips"));
+    }
+
+}
